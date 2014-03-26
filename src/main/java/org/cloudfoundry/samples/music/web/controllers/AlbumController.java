e846b34c07bdbab8826c8d782727101e1bac4972
Scott Frederick <scottyfred@gmail.com>
2014-03-26 15:57:27 -0700

    Added a controller to force the app to exit. Added app-level logging to the AlbumController.

--- src/main/java/org/cloudfoundry/samples/music/web/controllers/AlbumController.java
+++ src/main/java/org/cloudfoundry/samples/music/web/controllers/AlbumController.java
@@ -2,6 +2,8 @@ package org.cloudfoundry.samples.music.web.controllers;
 
 import org.cloudfoundry.samples.music.domain.Album;
 import org.cloudfoundry.samples.music.repositories.AlbumRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping(value = "/albums")
 public class AlbumController {
+    private static final Logger logger = LoggerFactory.getLogger(AlbumController.class);
     private AlbumRepository repository;
 
     @Autowired
@@ -27,24 +30,28 @@ public class AlbumController {
     @ResponseBody
     @RequestMapping(method = RequestMethod.PUT)
     public Album add(@RequestBody @Valid Album album) {
+        logger.info("Adding album " + album.getId());
         return repository.save(album);
     }
 
     @ResponseBody
     @RequestMapping(method = RequestMethod.POST)
     public Album update(@RequestBody @Valid Album album) {
+        logger.info("Updating album " + album.getId());
         return repository.save(album);
     }
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Album getById(@PathVariable String id) {
+        logger.info("Getting album " + id);
         return repository.findOne(id);
     }
 
     @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteById(@PathVariable String id) {
+        logger.info("Deleting album " + id);
         repository.delete(id);
     }
 }
